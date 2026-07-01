23a4462e69566d420e72112f3767dbba0fbf0d28
Poltergeist0828l <83198755a@gmail.com>
2026-07-01 16:18:12 -0300

    Implementar

--- app/__init__.py
+++ app/__init__.py
@@ -23,18 +23,3 @@ migrate = Migrate(app, db)
 from app import models
 from app import auth
 from app import routes
-
-CATEGORIAS_PADRAO = [
-    "Ficção", "Não Ficção", "Fantasia", "Ficção Científica", "Distopia",
-    "Suspense", "Thriller", "Terror", "Ação", "Aventura", "Romance",
-    "Drama", "Poesia", "Conto", "Crônica", "Policial", "Histórico",
-    "Biografia", "Autoajuda", "Infantil", "Juvenil",
-]
-
-with app.app_context():
-    db.create_all()
-    nomes_existentes = {c.nome.lower() for c in models.Categoria.query.all()}
-    for nome in CATEGORIAS_PADRAO:
-        if nome.lower() not in nomes_existentes:
-            db.session.add(models.Categoria(nome=nome))
-    db.session.commit()
